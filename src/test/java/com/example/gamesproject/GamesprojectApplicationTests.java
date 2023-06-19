62862c5fa57ac4a81ce10a4a7d87ace83e304208
Matti01 <leporem@bzz.ch>
2023-06-19 13:28:43 +0200

    Creation of Project Structure

---
+++ src/test/java/com/example/gamesproject/GamesprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
